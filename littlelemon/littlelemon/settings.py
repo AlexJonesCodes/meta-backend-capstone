2aa053e7207d10b56d4f0cf4328737cf5e7c9961
Alexander Jones <89351310+AlexJonesCodes@users.noreply.github.com>
2024-09-15 22:10:25 +0100

    added models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'LittleLemon',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
